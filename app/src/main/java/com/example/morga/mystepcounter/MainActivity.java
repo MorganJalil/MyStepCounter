386feb5e48a8752c750681a2d2f459c3a80ff823
Moggelicious <Moggelicious@hotmail.com>
2017-08-22 21:03:22 +0200

    0.0.2 setting up API Google Fit

--- app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
+++ app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.morga.mystepcounter;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.request.OnDataPointListener;
 
@@ -21,8 +27,40 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
         protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            if (savedInstanceState != null) {
+                authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+            }
+
+            mApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Fitness.SENSORS_API)
+                    .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
     }
 
+        @Override
+        protected void onStart() {
+            super.onStart();
+            mApiClient.connect();
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_OAUTH) {
+                authInProgress = false;
+                if (resultCode == RESULT_OK) {
+                    if (!mApiClient.isConnecting() && !mApiClient.isConnected()) {
+                        mApiClient.connect();
+                    }
+                } else if (resultCode == RESULT_CANCELED) {
+                    Log.e("GoogleFit", "RESULT_CANCELED");
+                }
+            } else {
+                Log.e("GoogleFit", "requestCode NOT request_oauth");
+            }
+        }
         @Override
         public void onConnected (Bundle bundle){
     }
@@ -33,6 +71,16 @@ public class MainActivity extends AppCompatActivity implements OnDataPointListen
 
         @Override
         public void onConnectionFailed (ConnectionResult connectionResult){
+            if( !authInProgress) {
+                try {
+                    authInProgress = true;
+                    connectionResult.startResolutionForResult(MainActivity.this, REQUEST_OAUTH);
+                } catch (IntentSender.SendIntentException e) {
+
+                }
+            } else {
+                Log.e( "GoogleFit", "authInProgress");
+            }
     }
 
         @Override
