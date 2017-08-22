764853431ff0e042f6a2381eb15e6303b7e5b795
Moggelicious <Moggelicious@hotmail.com>
2017-08-22 10:52:04 +0200

    0.0.1 setting up API Google Fit

--- app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
+++ app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
@@ -3,11 +3,34 @@ package com.example.morga.mystepcounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.request.OnDataPointListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity implements OnDataPointListener,
+    GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener {
+
+        @Override
+
+        protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+        @Override
+        public void onConnected (Bundle bundle){
+    }
+        @Override
+        public void onConnectionSuspended ( int i){
+    }
+
+        @Override
+        public void onConnectionFailed (ConnectionResult connectionResult){
+    }
+
+        @Override
+        public void onDataPoint (DataPoint dataPoint){
+        }
+    }
