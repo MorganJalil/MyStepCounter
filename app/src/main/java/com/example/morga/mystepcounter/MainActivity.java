552ff90a2288a365e490eeea6c3f7b22aef7ed0e
Morgan <Moggelicious@hotmail.com>
2017-09-04 17:07:20 +0200

    0.0.8 - history week getting data in .log

--- app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
+++ app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
@@ -31,13 +31,20 @@ import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessStatusCodes;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.result.DailyTotalResult;
+import com.google.android.gms.fitness.result.DataReadResult;
 import com.google.android.gms.fitness.result.ListSubscriptionsResult;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -245,10 +252,123 @@ import java.util.concurrent.TimeUnit;
             
         }
 
+     private void showDataSet(DataSet dataSet) {
+
+         Log.e("History", "Data returned for Data type: " + dataSet.getDataType().getName());
+
+         DateFormat dateFormat = DateFormat.getDateInstance();
+
+         DateFormat timeFormat = DateFormat.getTimeInstance();
+
+
+         for (DataPoint dp : dataSet.getDataPoints()) {
+
+             Log.e("History", "Data point:");
+
+             Log.e("History", "\tType: " + dp.getDataType().getName());
+
+             Log.e("History", "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+
+             Log.e("History", "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " " + timeFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+
+             for (Field field : dp.getDataType().getFields()) {
+
+                 Log.e("History", "\tField: " + field.getName() +
+
+                         " Value: " + dp.getValue(field));
+
+             }
+
+         }
+
+     }
+
+     private void displayLastWeeksData() {
+
+         Calendar cal = Calendar.getInstance();
+
+         Date now = new Date();
+
+         cal.setTime(now);
+
+         long endTime = cal.getTimeInMillis();
+
+         cal.add(Calendar.WEEK_OF_YEAR, -1);
+
+         long startTime = cal.getTimeInMillis();
+
+
+
+         java.text.DateFormat dateFormat = DateFormat.getDateInstance();
+
+         Log.e("History", "Range Start: " + dateFormat.format(startTime));
+
+         Log.e("History", "Range End: " + dateFormat.format(endTime));
+
+
+
+         //Check how many steps were walked and recorded in the last 7 days
+
+         DataReadRequest readRequest = new DataReadRequest.Builder()
+
+                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+
+                 .bucketByTime(1, TimeUnit.DAYS)
+
+                 .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+
+                 .build();
+
+
+
+         DataReadResult dataReadResult = Fitness.HistoryApi.readData(mClient, readRequest).await(1, TimeUnit.MINUTES);
+
+
+
+         //Used for aggregated data
+
+         if (dataReadResult.getBuckets().size() > 0) {
+
+             Log.e("History", "Number of buckets: " + dataReadResult.getBuckets().size());
+
+             for (Bucket bucket : dataReadResult.getBuckets()) {
+
+                 List<DataSet> dataSets = bucket.getDataSets();
+
+                 for (DataSet dataSet : dataSets) {
+
+                     showDataSet(dataSet);
+
+                 }
+
+             }
+
+         }
+
+         //Used for non-aggregated data
+
+         else if (dataReadResult.getDataSets().size() > 0) {
+
+             Log.e("History", "Number of returned DataSets: " + dataReadResult.getDataSets().size());
+
+             for (DataSet dataSet : dataReadResult.getDataSets()) {
+
+                 showDataSet(dataSet);
+
+             }
+
+         }
+
+     }
+
 
 
         private void readData() {
             new VerifyDataTask().execute();
+        }
+
+        private void readWeek() {
+            new ViewWeekStepCountTask().execute();
         }
 
         @Override
@@ -271,6 +391,10 @@ import java.util.concurrent.TimeUnit;
             }
             if (id == R.id.pause) {
                 cancelSubscriptions();
+                return true;
+            }
+            if (id == R.id.week) {
+                readWeek();
                 return true;
             }
             return super.onOptionsItemSelected(item);
@@ -303,4 +427,15 @@ import java.util.concurrent.TimeUnit;
          }
 
 
+     private class ViewWeekStepCountTask extends AsyncTask<Void, Void, Void> {
+
+         protected Void doInBackground(Void... params) {
+
+             displayLastWeeksData();
+
+             return null;
+
+         }
+
+     }
  }
