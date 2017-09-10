6603e13fca78b1eb751fa7d20da47e52f3b3e8ce
Morgan <Moggelicious@hotmail.com>
2017-09-10 12:15:23 +0200

    0.1.0 data to fragment

---
+++ app/src/main/java/com/example/morga/mystepcounter/IFragmentToActivity.java
@@ -0,0 +1,15 @@
+package com.example.morga.mystepcounter;
+
+/**
+ * Created by Morga on 2017-09-10.
+ */
+
+public interface IFragmentToActivity {
+
+    void showToast(String msg);
+
+
+
+    void communicateToFragment2();
+
+}
--- app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
+++ app/src/main/java/com/example/morga/mystepcounter/MainActivity.java
@@ -1,29 +1,20 @@
 package com.example.morga.mystepcounter;
 
 
-import android.content.Intent;
+
 import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
-import android.os.Looper;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -59,14 +50,14 @@ import java.util.logging.Handler;
  * authenticate a user with Google Play Services.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IFragmentToActivity {
 
     public static final String TAG = "StepCounter";
     private GoogleApiClient mClient = null;
 
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+    private PagerAdapter adapter;
+    private TabLayout tabLayout;
 
 
     @Override
@@ -74,18 +65,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
 
+        ArrayList<String> tabs = new ArrayList<>();
+        tabs.add("DAILY STEPS");
+        tabs.add("LAST SEVEN DAYS");
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        adapter = new PagerAdapter(getSupportFragmentManager(),tabs);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
 
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -123,79 +119,39 @@ public class MainActivity extends AppCompatActivity {
         }
         if (id == R.id.week) {
             readWeek();
+
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
 
-        public PlaceholderFragment() {
-        }
+    public void showToast(String msg) {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+
+    @Override
+
+    public void communicateToFragment2() {
+
+        TabFragment2 fragment = (TabFragment2) adapter.getFragment(1);
+
+        if (fragment != null) {
+
+            fragment.fragmentCommunication();
+
+        } else {
+
+            Log.i(TAG, "Fragment 2 is not initialized");
+
         }
 
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "TODAY";
-                case 1:
-                    return "LAST WEEK";
-                case 2:
-                    return "SETTINGS";
-            }
-            return null;
-        }
     }
-
 
 
     /**
